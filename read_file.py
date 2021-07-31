111efd603f9082dafb5b1daa38348516eb5dc0a3
kunjpatel1402 <75675988+kunjpatel1402@users.noreply.github.com>
2021-08-01 01:22:24 +0530

    Update read_file.py

--- read_file.py
+++ read_file.py
@@ -1,34 +1,6 @@
 import sys
 from binary_to_image import binary_to_image
 from image_to_binary import image_to_binary
-'''
-binary_data = ""
-with open(r"","rb") as file:
-    byte = file.read(2)
-    count=0
-    while byte:
-        s='{0:016b}'.format(int.from_bytes(byte, byteorder=sys.byteorder))
-        #print(byte,int(s, 2).to_bytes(len(s) // 8, byteorder='little'))
-        binary_data += s
-        byte=file.read(2)
-        count+=1
-    file.close()
-
-binary_to_image(r"",binary_data)
-print(image_to_binary(r"")==binary_data)
-
-with open(r"    ","wb") as file:
-    i = 0
-    while i < len(binary_data):
-        s=binary_data[i:i+16]
-        try:
-            file.write(int(s, 2).to_bytes(len(s) // 8, byteorder='little'))
-        except:
-            print(s)
-            break
-        i+=16
-    file.close()
-'''
 
 def file_to_binary(file_path):
     binary_data = ""
